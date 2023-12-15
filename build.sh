daa6771bb12895759af81d95a326f31da56151bc
Hui Mi <mihui@gmail.com>
2023-12-15 14:26:21 +0800

    only use compose file

--- build.sh
+++
@@ -1,7 +0,0 @@
-
-pushd ss
-sudo docker build . -t ss --network=host
-popd
-pushd kcp/
-sudo docker build . -t kcp --network=host
-popd
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sudo docker rm -fv ss kcp
-sudo docker-compose up -d
